4e2d21931ca7e9908ce8e4d8c466bae6f9f86bf7
gastonlcruz <gastonlcruz@gmail.com>
2024-01-30 16:46:28 -0300

    agregado nav bar

--- glcporfolio/src/Components/ItemListContainer/ItemListContainer.jsx
+++ glcporfolio/src/Components/ItemListContainer/ItemListContainer.jsx
@@ -7,19 +7,29 @@ function ItemListContainer({greeting}) {
     const scrollRef = useRef(null);
 
     useEffect(() => {
-        const scroll = new LocomotiveScroll({
+        const scroll2 = new LocomotiveScroll({
             el: document.querySelector('[data-scroll-container]'),
             smooth: true,
             multiplier:.5
     });
 
     return () => {
-        scroll.destroy();
+        scroll2.destroy();
     }
     
 },[]);
+
+
+
+
+   
+   
+
     
-    const newLocal = "c-section";
+
+
+    
+    //const newLocal = "c-section";
     return (
         <div data-scroll-container>
         <section data-scroll-section>
@@ -61,44 +71,44 @@ function ItemListContainer({greeting}) {
             </section>
 
             <section data-scroll-section>
-                <div class="c-section" id="scroll-direction">
-                    <div class="c-direction-block_wrapper">
-                        <div class="c-section_infos -padding" data-scroll data-scroll-sticky data-scroll-target="#scroll-direction">
-                            <div class="c-section_infos_inner" data-scroll>
+                <div id="scroll-direction">
+                    <div >
+                        <div data-scroll data-scroll-sticky data-scroll-target="#scroll-direction">
+                            <div data-scroll>
                                 <h3>
                                     02. 
                                     Scroll direction
                                 </h3>
-                                <div class="c-sections_infos_text u-text">
+                                <div >
                                     <p>
                                         And if that wasn't enough, make 'em go backwards. Or upwards. Or downwards!
                                     </p>
                                 </div>
                             </div>
                         </div>
-                        <div class="c-direction-block" id="direction">
-                            <div class="c-direction-block_item -one">
-                                <span class="c-direction-block_item_inner" data-scroll data-scroll-direction="horizontal" data-scroll-speed="6" data-scroll-target="#direction">
+                        <div id="direction">
+                            <div >
+                                <span data-scroll data-scroll-direction="horizontal" data-scroll-speed="6" data-scroll-target="#direction">
                                     I'm moving in this direction
                                 </span>
                             </div>
-                            <div class="c-direction-block_item -two">
-                                <span class="c-direction-block_item_inner" data-scroll data-scroll-direction="horizontal" data-scroll-speed="-8" data-scroll-target="#direction">
+                            <div >
+                                <span data-scroll data-scroll-direction="horizontal" data-scroll-speed="-8" data-scroll-target="#direction">
                                     And in this direction
                                 </span>
                             </div>
-                            <div class="c-direction-block_item -three">
-                                <span class="c-direction-block_item_inner" data-scroll data-scroll-direction="horizontal" data-scroll-speed="9" data-scroll-target="#direction" data-scroll-delay="0.05">
+                            <div >
+                                <span data-scroll data-scroll-direction="horizontal" data-scroll-speed="9" data-scroll-target="#direction" data-scroll-delay="0.05">
                                     Sooo customizable. Right?
                                 </span>
                             </div>
-                            <div class="c-direction-block_item -four">
-                                <span class="c-direction-block_item_inner" data-scroll data-scroll-direction="horizontal" data-scroll-speed="15" data-scroll-target="#direction">
+                            <div >
+                                <span data-scroll data-scroll-direction="horizontal" data-scroll-speed="15" data-scroll-target="#direction">
                                     I can also go in this direction
                                 </span>
                             </div>
-                            <div class="c-direction-block_item -five">
-                                <span class="c-direction-block_item_inner" data-scroll data-scroll-direction="horizontal" data-scroll-speed="-20" data-scroll-target="#direction" data-scroll-delay="0.05">
+                            <div >
+                                <span data-scroll data-scroll-direction="horizontal" data-scroll-speed="-20" data-scroll-target="#direction" data-scroll-delay="0.05">
                                     Ok, enough!!!!
                                 </span>
                             </div>
